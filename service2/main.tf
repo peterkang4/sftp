17139ed0f6c676ee623c238c8f9823f620c53624
Peter Kang <peterkang@peters-mbp.lan>
2021-01-07 15:37:31 -0500

    Updated to add route53

--- service2/main.tf
+++ service2/main.tf
@@ -1,155 +1,106 @@
 # IAM
-resource "aws_iam_instance_profile" "sftp_instance_profile" {
-  name = "sftp_instance_profile"
-  role = aws_iam_role.peter-sftp-service-role.name
-  path = "/"
-}
+data "aws_iam_policy_document" "transfer_server_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
 
-resource "aws_iam_role" "peter-sftp-service-role" {
-  name               = "peter-sftp-service-role"
-  description        = "SFTP service role for S3"
-  path               = "/"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "transfer.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": "AllowTransfer"
+    principals {
+      type        = "Service"
+      identifiers = ["transfer.amazonaws.com"]
     }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "peter-sftp-service-policy" {
-  name        = "peter-sftp-service-policy"
-  policy      = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:List*",
-        "s3:Get*",
-        "s3:Put*"
-      ],
-      "Effect": "Allow",
-      "Resource": ["arn:aws:s3:::peterkang-sftp-bucket", "arn:aws:s3:::peterkang-sftp-bucket/*"]
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy_attachment" "peter-sftp-policy-role-attachment" {
-  name       = "peter-sftp-policy-role-attachment"
-  roles      = [ aws_iam_role.peter-sftp-service-role.name ]
-  policy_arn = aws_iam_policy.peter-sftp-service-policy.arn
-}
-
-# EC2
-resource "aws_security_group" "sftp-securitygroup" {
-  name          = "sftp-securitygroup"
-  vpc_id        = var.VPC_ID
-  ingress {
-    description = "SFTP/SSH"
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTP"
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTPS"
-    protocol    = "tcp"
-    from_port   = 443
-    to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags          = {
-    Name        = "peter-sftp"
-    User        = "Peter Kang"
-    Project     = "Peter SFTP"
   }
 }
 
-
-###################
-
-
-resource "aws_launch_configuration" "peter-sftp-lc" {
-  name_prefix   = "peter-sftp-lc"
-  image_id      = "ami-0be2609ba883822ec"
-  instance_type = "t2.micro"
-  iam_instance_profile = "sftp_instance_profile"
-  key_name = var.KEY_NAME
-  associate_public_ip_address = true
-  security_groups = [ aws_security_group.sftp-securitygroup.id ]
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  # Block device mappings
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 10
-    delete_on_termination = true
-  }
-
-  # User Data
-
-}
-
-resource "aws_autoscaling_group" "peter-sftp-asg" {
-  name                = "peter-sftp-asg"
-  launch_configuration = aws_launch_configuration.peter-sftp-lc.name
-  min_size            = 1
-  max_size            = 1
-  vpc_zone_identifier  = [ var.SUBNET1, var.SUBNET2 ]
-
-  lifecycle {
-    create_before_destroy = true
+data "aws_iam_policy_document" "transfer_server_assume_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:DeleteObject",
+      "s3:DeleteObjectVersion",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:ListBucket",
+      "s3:PutObject",
+    ]
+    resources = [
+      var.S3_BUCKET_ARN,
+      var.S3_BUCKET_OBJECT_ARN
+    ]
   }
 }
 
+data "aws_iam_policy_document" "transfer_server_to_cloudwatch_assume_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "transfer_server_role" {
+  name               = "${var.TRANSFER_SERVER_NAME}-transfer_server_role"
+  assume_role_policy = data.aws_iam_policy_document.transfer_server_assume_role.json
+  tags               = {
+    Project          = "peter-sftp-tf"
+  }
+}
+
+resource "aws_iam_role_policy" "transfer_server_policy" {
+  name   = "${var.TRANSFER_SERVER_NAME}-transfer_server_policy"
+  role   = aws_iam_role.transfer_server_role.name
+  policy = data.aws_iam_policy_document.transfer_server_assume_policy.json
+}
+
+resource "aws_iam_role_policy" "transfer_server_to_cloudwatch_policy" {
+  name   = "${var.TRANSFER_SERVER_NAME}-transfer_server_to_cloudwatch_policy"
+  role   = aws_iam_role.transfer_server_role.name
+  policy = data.aws_iam_policy_document.transfer_server_to_cloudwatch_assume_policy.json
+}
+
+# SFTP Server
+resource "aws_transfer_server" "transfer_server" {
+  identity_provider_type = "SERVICE_MANAGED"
+  logging_role           = aws_iam_role.transfer_server_role.arn
+  endpoint_type          = "PUBLIC"
+  
+  tags                   = {
+    Project              = "peter-sftp-tf"
+  }
+}
+
+resource "aws_transfer_user" "transfer_server_user" {
+  server_id      = aws_transfer_server.transfer_server.id
+  user_name      = var.TRANSFER_SERVER_USERNAME
+  role           = aws_iam_role.transfer_server_role.arn
+  home_directory = "/${var.S3_BUCKET_NAME}"
+}
+
+resource "aws_transfer_ssh_key" "transfer_server_ssh_key" {
+  server_id = aws_transfer_server.transfer_server.id
+  user_name = aws_transfer_user.transfer_server_user.user_name
+  body      = file("sftp.pub")
+}
 
 
+# Route 53
+resource "aws_route53_zone" "peter_sftp_zone" {
+  name      = "pkang-sftp.com"
+  tags      = {
+    Project = "peter-sftp-tf"
+  }
+}
 
-###################
-
-# resource "aws_instance" "peter_sftp" {
-#   ami                    = "ami-0be2609ba883822ec"  # Amazon Linux, us-east-1
-#   instance_type          = "t2.micro"
-#   subnet_id              = var.SUBNET1
-#   key_name               = var.KEY_NAME
-#   iam_instance_profile    = "sftp_instance_profile"
-#   vpc_security_group_ids = [ aws_security_group.sftp-securitygroup.id ]
-#   associate_public_ip_address = true
-#   root_block_device {
-#     volume_type = "gp2"
-#     volume_size = 10
-#   }
-#   tags   = {
-#     Name = "peter_sftp"
-#     User        = "Peter Kang"
-#     Project     = "Peter SFTP"
-#   }
-# }
+resource "aws_route53_record" "cname-record" {
+  zone_id = aws_route53_zone.peter_sftp_zone.zone_id
+  name    = "test"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [ aws_transfer_server.transfer_server.endpoint ]
+}
--- service2/variables.tf
+++ service2/variables.tf
@@ -7,3 +7,8 @@ variable "KEY_NAME" {}
 variable "VPC_ID" {}
 variable "SUBNET1" {}
 variable "SUBNET2" {}
+variable "S3_BUCKET_NAME" {}
+variable "S3_BUCKET_ARN" {}
+variable "S3_BUCKET_OBJECT_ARN" {}
+variable "TRANSFER_SERVER_NAME" {}
+variable "TRANSFER_SERVER_USERNAME" {}
